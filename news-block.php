df301c9ccb9b36d9c223f71036ebca36044da998
Stephen Schrauger <stephen@schrauger.com>
2020-12-03 11:41:13 -0500

    updates simple-html-dom to latest version

--- news-block.php
+++ news-block.php
@@ -3,7 +3,7 @@
 Plugin Name: News Block
 Plugin URI: https://github.com/schrauger/news-block
 Description: WordPress Block for embedding COM and UCF Health news articles.
-Version: 1.4.6
+Version: 1.5.0
 Author: Stephen Schrauger
 Author URI: https://github.com/schrauger/news-block
 License: GPL2
